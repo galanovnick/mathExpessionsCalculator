bee323304a35f126d98386d2d53ff4422a4b6555
Nikolay Galanov <galanov.nick@gmail.com>
2016-06-22 18:31:03 +0300

    Implemented main expression calculator.

--- src/main/java/calculator/MathExpressionsCalculator.java
+++ src/main/java/calculator/MathExpressionsCalculator.java
@@ -2,7 +2,16 @@ package calculator;
 
 import calculator.exception.CalculationException;
 
+/**
+ * Public API of mathematical expressions calculator.
+ */
 public interface MathExpressionsCalculator {
 
+    /**
+     * Evaluates input math expression.
+     * @param mathExpression expression represented by character string
+     * @return evaluated floating number
+     * @throws CalculationException signals about illegal syntax of input string
+     */
     double evaluate(String mathExpression) throws CalculationException;
 }
--- src/main/java/calculator/impl/MathExpressionsCalculatorImpl.java
+++ src/main/java/calculator/impl/MathExpressionsCalculatorImpl.java
@@ -2,10 +2,72 @@ package calculator.impl;
 
 import calculator.MathExpressionsCalculator;
 import calculator.exception.CalculationException;
+import calculator.impl.abstractstatemachine.AbstractCharacterExpressionResolver;
+import calculator.impl.context.InputContext;
+import calculator.impl.context.InputMathExpressionContext;
+import calculator.impl.context.OutputContext;
+import calculator.impl.context.OutputMathExpressionContext;
+import calculator.impl.parser.ExpressionParser;
+import calculator.impl.parser.ExpressionParsersContainer;
+import calculator.impl.parser.FinishParser;
+import calculator.impl.parser.NumberParser;
+import calculator.impl.stateenum.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class MathExpressionsCalculatorImpl implements MathExpressionsCalculator {
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import static calculator.impl.stateenum.State.*;
+
+public class MathExpressionsCalculatorImpl
+        extends AbstractCharacterExpressionResolver<CalculationException, State>
+        implements MathExpressionsCalculator {
+
+    private final static Logger log = LoggerFactory.getLogger(MathExpressionsCalculator.class);
+
+    public MathExpressionsCalculatorImpl() {
+        super(registerTransitions(), registerParsers());
+    }
 
     public double evaluate(String mathExpression) throws CalculationException {
-        return 0;
+        InputMathExpressionContext inputContext =
+                new InputMathExpressionContext(mathExpression, START, FINISH);
+        OutputMathExpressionContext outputContext =
+                new OutputMathExpressionContext();
+
+        run(inputContext, outputContext);
+
+        return outputContext.getResult();
+    }
+
+    @Override
+    public void deadlock(InputContext inputContext) throws CalculationException {
+        if (log.isWarnEnabled()) {
+            log.warn("Input expression is invalid. Symbol at "
+                    + inputContext.getPointer() + " position unresolved.");
+        }
+
+
+        throw new CalculationException("Cannot resolve symbol at "
+                + inputContext.getPointer() + "position", inputContext.getPointer());
+    }
+
+    private static Map<State, EnumSet<State>> registerTransitions() {
+        return new HashMap<State, EnumSet<State>>() {{
+
+            put(START, EnumSet.of(NUMBER));
+            put(NUMBER, EnumSet.of(FINISH));
+        }};
+    }
+
+    private static HashMap<State, ExpressionParser> registerParsers() {
+        return new HashMap<State, ExpressionParser>() {{
+
+            put(NUMBER, new NumberParser());
+            put(FINISH, new FinishParser());
+        }};
     }
 }
