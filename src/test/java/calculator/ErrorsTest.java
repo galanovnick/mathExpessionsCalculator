132ab54f9a5e92f94d542cd217ad73b0434e8cd1
Nikolay Galanov <galanov.nick@gmail.com>
2016-06-22 10:54:14 +0300

    Added errors tests.

---
+++ src/test/java/calculator/ErrorsTest.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import calculator.exception.CalculationException;
+import calculator.impl.MathExpressionsCalculatorImpl;
+import org.junit.Test;
+
+public class ErrorsTest {
+
+    MathExpressionsCalculator calculator = new MathExpressionsCalculatorImpl();
+
+    @Test(expected = CalculationException.class)
+    public void testInvalidCharacter() throws CalculationException {
+        calculator.evaluate("|");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyString() throws CalculationException {
+        calculator.evaluate(null);
+    }
+
+    @Test(expected = CalculationException.class)
+    public void testOpenBracketError() throws CalculationException {
+        calculator.evaluate("(1+3");
+    }
+
+    @Test(expected = CalculationException.class)
+    public void testCloseBracketError() throws CalculationException {
+        calculator.evaluate("1+3)");
+    }
+}
