164397032440280d2075dfa7576a30da74493190
Nikolay Galanov <galanov.nick@gmail.com>
2016-06-23 13:55:49 +0300

    Added multiply and divide operators.

---
+++ src/main/java/calculator/impl/tokens/Multiply.java
@@ -0,0 +1,18 @@
+package calculator.impl.tokens;
+
+import static calculator.impl.tokens.Priority.MEDIUM;
+
+public class Multiply extends AbstractBinaryOperator {
+
+    private final Priority priority = MEDIUM;
+
+    @Override
+    public double execute(double leftOperand, double rightOperand) {
+        return leftOperand * rightOperand;
+    }
+
+    @Override
+    public Priority getPriority() {
+        return priority;
+    }
+}
